f9d0552bb6f5bf1cdb02f33228e3e0cf0c304521
Corey Hansen <chansen@e1z2r7p16.42.us.org>
2016-10-07 15:44:40 -0700

    strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: chansen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/25 04:19:44 by chansen           #+#    #+#             */
-/*   Updated: 2016/09/25 04:19:45 by chansen          ###   ########.fr       */
+/*   Updated: 2016/10/07 15:41:48 by chansen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strdup(const char *s)
 	ns = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
 	if (ns == NULL)
 		return (NULL);
-	strcpy(ns, s);
+	ft_strcpy(ns, s);
 	return (ns);
 	free(ns);
 }
