a1ff426f0315ac253137b252ae72b88aedf772ed
Corey Hansen <chansen@e1z2r7p20.42.us.org>
2016-10-20 16:50:33 -0700

    libft

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: chansen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/27 15:56:00 by chansen           #+#    #+#             */
-/*   Updated: 2016/09/27 15:56:02 by chansen          ###   ########.fr       */
+/*   Updated: 2016/10/11 14:14:00 by chansen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ char	*ft_strnew(size_t size)
 	str = (char *)ft_memalloc(size + 1);
 	if (!(str))
 		return (NULL);
-	str[size + 1] = '\0';
 	ft_bzero(str, size);
 	return (str);
 }
